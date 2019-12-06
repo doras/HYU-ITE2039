3f8b8032518829f71eb99e54f947b98cc718839e
2018008659 <shoon0113@naver.com>
2019-12-06 22:27:07 +0900

    homework#2: minimum spanning tree, dijkstra

---
+++ mst.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct Edge {
+    int src;
+    int dst;
+    int cost;
+
+    Edge(int src, int dst, int cost) : src(src), dst(dst), cost(cost) {}
+
+    bool operator< (const Edge &other) const {
+        return cost < other.cost;
+    }
+};
+
+int find_set(int *set, int target) {
+    
+    if (target == set[target]) {
+        return target;
+    }
+
+    set[target] = find_set(set, set[target]);
+    return set[target];
+}
+
+void union_set(int *set, int u, int v) {
+    int parent_u = find_set(set, u);
+    int parent_v = find_set(set, v);
+
+    set[parent_u] = parent_v;
+}
+
+
+int mst(int size, std::vector<Edge> &edges) {
+    int *disjoint_set = new int[size];
+
+    int result_cost = 0;
+
+    for (int i = 0; i < size; ++i) {
+        disjoint_set[i] = i;
+    }
+
+    std::sort(edges.begin(), edges.end());
+
+    for (std::vector<Edge>::iterator it = edges.begin(); it != edges.end(); ++it) {
+        if (find_set(disjoint_set, it->src - 1) != find_set(disjoint_set, it->dst - 1)) {
+            union_set(disjoint_set, it->src - 1, it->dst - 1);
+            result_cost += it->cost;
+        }
+    }
+
+    delete[] disjoint_set;
+
+    return result_cost;
+}
+
+int main() {
+    std::string line;
+    std::istringstream sin;
+    std::vector<Edge> edges;
+    int num_cities, num_city_pairs;
+    int city0, city1, cost;
+
+    std::getline(std::cin, line);
+    while (line != "") {
+        sin.str(line);
+        sin.clear();
+
+        sin >> num_cities >> num_city_pairs;
+
+        if (sin.fail()) {
+            return EXIT_FAILURE;
+        }
+
+        for (int i = 0; i < num_city_pairs; ++i) {
+            std::getline(std::cin, line);
+            sin.str(line);
+            sin.clear();
+
+            sin >> city0 >> city1 >> cost;
+            if (sin.fail() || city0 <= 0 || city1 <= 0 || cost <= 0 || city0 > num_cities || city1 > num_cities || city0 == city1) {
+                return EXIT_FAILURE;
+            }
+
+            edges.push_back(Edge(city0, city1, cost));
+        }
+        std::cout << mst(num_cities, edges) << std::endl;
+        edges.clear();
+        std::getline(std::cin, line);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ shortest_path.cpp
@@ -0,0 +1,118 @@
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <queue>
+#include <sstream>
+
+struct Graph {
+    int **edges;
+    int size;
+
+    Graph(int size) : size(size), edges(new int*[size]) {
+        for (size_t i = 0; i < size; ++i) {
+            edges[i] = new int[size];
+            std::fill_n(edges[i], size, -1);
+        }
+    }
+
+    void insert(int first, int second, int cost) {
+        --first;
+        --second;
+        edges[first][second] = cost;
+        edges[second][first] = cost;
+    }
+};
+
+struct HeapNode {
+    int vid;
+    int dist;
+
+    HeapNode(int vid, int dist) : vid(vid), dist(dist) {}
+};
+
+struct CompareHeap {
+    bool operator() (const HeapNode &lhs, const HeapNode &rhs) {
+        return lhs.dist > rhs.dist;
+    }
+};
+
+int dijkstra(Graph &graph, int src, int dst) {
+    --src;
+    --dst;
+    std::priority_queue<HeapNode, std::vector<HeapNode>, CompareHeap> heap;
+    bool *set = new bool[graph.size]();
+    int *distances = new int[graph.size];
+
+    std::fill_n(distances, graph.size, INT_MAX);
+
+    for (size_t i = 0; i < graph.size; ++i) {
+        heap.push(HeapNode(i, INT_MAX));
+    }
+    heap.push(HeapNode(src, 0));
+
+    while (!heap.empty()) {
+        HeapNode min = heap.top();
+        heap.pop();
+        if (set[min.vid]) {
+            continue;
+        }
+
+        set[min.vid] = true;
+        distances[min.vid] = min.dist;
+
+        for (size_t i = 0; i < graph.size; ++i) {
+            if (!set[i] && graph.edges[min.vid][i] > 0) {
+                heap.push(HeapNode(i, min.dist + graph.edges[min.vid][i]));
+            }
+        }
+    }
+    
+    int result = distances[dst];
+
+    delete[] set;
+    delete[] distances;
+
+    return result;
+}
+
+
+int main() {
+    std::string line;
+    std::istringstream sin;
+    int num_nodes, num_edges;
+    int node0, node1, distance;
+
+
+    std::getline(std::cin, line);
+    while (line != "") {
+        sin.str(line);
+        sin.clear();
+
+        sin >> num_nodes >> num_edges;
+
+        if (sin.fail()) {
+            return EXIT_FAILURE;
+        }
+
+        Graph graph = Graph(num_nodes);
+
+        for (int i = 0; i < num_edges; ++i) {
+            std::getline(std::cin, line);
+            sin.str(line);
+            sin.clear();
+
+            
+
+            sin >> node0 >> node1 >> distance;
+            if (sin.fail() || node0 <= 0 || node1 <= 0 || distance <= 0 || node0 > num_nodes || node1 > num_nodes || node0 == node1) {
+                return EXIT_FAILURE;
+            }
+
+            graph.insert(node0, node1, distance);
+        }
+        std::cout << dijkstra(graph, 1, num_nodes) << std::endl;
+        std::getline(std::cin, line);
+    }
+
+    return EXIT_SUCCESS;
+}
